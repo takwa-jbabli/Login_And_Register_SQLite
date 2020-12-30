8dc4056ee5da55a69dabc4d988271d4d82978c3d
takwa-jbabli <takoujb@gmail.com>
2020-12-30 22:16:20 +0100

    Initial commit

---
+++ app/src/main/java/com/example/login_and_register_sqlite/Login.java
@@ -0,0 +1,42 @@
+package com.example.login_and_register_sqlite;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+    EditText e1,e2;
+    Button b1 ;
+    DatabaseHelper db ;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        db = new DatabaseHelper(this);
+        e1 = (EditText)findViewById(R.id.editText);
+        e2 = (EditText)findViewById(R.id.editText2);
+        b1 = (Button)findViewById(R.id.button2);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = e1.getText().toString();
+                String password = e2.getText().toString();
+                Boolean chkemailpass = db.emailpassword(email,password);
+                if (chkemailpass==true)
+                    Toast.makeText(getApplicationContext(),"Successfully Login ",Toast.LENGTH_SHORT).show();
+                else
+                    Toast.makeText(getApplicationContext(),"Wrong Email or PassWord",Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
+
+    }
+}
